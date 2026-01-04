d072450b6832d339ba6439ad5f2b700b78a05ce5
Geevee-101 <gongreashVoid@gmail.com>
2026-01-04 18:28:32 +0800

    Deployment ready for Cloudflare and Render

--- frontend/src/store/useAuthStore.ts
+++ frontend/src/store/useAuthStore.ts
@@ -5,7 +5,9 @@ import { AxiosError } from "axios";
 import { io, Socket } from "socket.io-client";
 
 const BASE_URL =
-  import.meta.env.MODE === "development" ? "http://localhost:3000" : "/";
+  import.meta.env.MODE === "development"
+    ? "http://localhost:3000"
+    : import.meta.env.VITE_API_URL || "/";
 
 interface AuthUser {
   name: string;
